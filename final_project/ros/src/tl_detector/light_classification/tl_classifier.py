42eccc83035628a3e842394e3249dde65ec7bd0c
robgineer <robgineer@gmail.com>
2018-07-04 15:49:01 +0200

    Updated path of ssd and squeezenet graphs

--- final_project/ros/src/tl_detector/light_classification/tl_classifier.py
+++ final_project/ros/src/tl_detector/light_classification/tl_classifier.py
@@ -19,7 +19,7 @@ class TLClassifier(object):
         if model_path is None:
             #rp = rospkg.RosPack()
             # model folder in tl_detector folder
-            model_path = os.path.join(os.pardir, 'model')
+            model_path = os.path.join(os.path.dirname(os.getcwd()), 'model')
 
         detection_model_path = os.path.join(model_path, 'model_detection.pb')
         classification_model_path = os.path.join(model_path, 'model_classification.pb')
